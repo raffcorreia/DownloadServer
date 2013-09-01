db113881aa20ed48539c107c0e149abc6f5b06f0
Rafael Correia <ralicoman@gmail.com>
2013-09-01 00:51:57 -0400

    Improvements
    -Created the custom error page
    -Created the LOG system

--- DownloadServer/Configuration.cs
+++ DownloadServer/Configuration.cs
@@ -5,11 +5,71 @@ namespace DownloadServer
 {
     public static class Configuration
     {
+        public static string BaseDir
+        {
+            get
+            {
+                string ret = AppDomain.CurrentDomain.BaseDirectory;
+                if (!ret.EndsWith("\\"))
+                {
+                    ret += "\\";
+                }
+                return ret;
+            }
+        }
+        public static bool ArePathsRelative
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ArePathsRelative"].ToLower() == "true";
+            }
+        }        
+        public static string LOGPath
+        {
+            get
+            {
+                string path = ConfigurationManager.AppSettings["LOGPath"];
+                if (path[0] == '\\')
+                {
+                    path = path.Substring(1);
+                }
+                if(!path.EndsWith("\\"))
+                {
+                    path += "\\";
+                }
+                
+                if (ArePathsRelative)
+                {
+                    return BaseDir + path;
+                }
+                else
+                {
+                    return path;
+                }
+            }
+        }        
         public static string FilesPath 
         {
             get
             {
-                return AppDomain.CurrentDomain.BaseDirectory + ConfigurationManager.AppSettings["FilesRelativePath"];
+                string path = ConfigurationManager.AppSettings["FilesPath"];
+                if (path[0] == '\\')
+                {
+                    path = path.Substring(1);
+                }
+                if (!path.EndsWith("\\"))
+                {
+                    path += "\\";
+                }
+
+                if (ArePathsRelative)
+                {
+                    return BaseDir + path;
+                }
+                else
+                {
+                    return path;
+                }
             }
         }
         public static string ConnectionString
---
+++ DownloadServer/Error/ErrorLog.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace DownloadServer.Error
+{
+    public class ErrorLog
+    {
+        private Exception _exception;
+        public Exception exception
+        {
+            get { return _exception; }
+            set { _exception = value; }
+        }
+        private HttpRequest _request;
+        public HttpRequest Request
+        {
+            get { return _request; }
+            set { _request = value; }
+        }
+        private int _status;
+        public int status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        public ErrorLog()
+        {
+            recordLOG();
+        }
+        public ErrorLog(Exception ex, HttpRequest request)
+        {
+            this.exception = ex;
+            this.Request = request;
+            if (!String.IsNullOrEmpty(Request["status"]))
+            {
+                status = int.Parse(Request["status"]);
+            }
+            recordLOG();
+        }
+
+        private void recordLOG()
+        {
+            string date = DateTime.UtcNow.ToString("yyyy_MM_dd");
+            string time = DateTime.UtcNow.TimeOfDay.ToString();
+            string msg = "";
+
+            if (exception != null)
+            {
+                msg = exception.Message;
+            }
+
+            recordSummary(date, time, msg);
+            recordDetail();
+        }
+
+        private void recordSummary(string date, string time, string msg)
+        {
+            string fileURL = Configuration.LOGPath + "Summary_" + date + "{0}.config";
+            FileInfo fi = new FileInfo(String.Format(fileURL, ""));
+            string namecomplement = "";
+            
+            int count = 1;
+            bool nameOK = false;
+            while (!nameOK)
+            {
+                if (fi.Exists)
+                {
+                    if (File.Exists(String.Format(fileURL, "(" + count + ")")) || fi.Length > 1048576)
+                    {
+                        namecomplement = "(" + count + ")";
+                        fi = new FileInfo(String.Format(fileURL, "(" + count + ")"));
+                    }
+                    else
+                    {
+                        nameOK = true;
+                    }
+                }
+                else
+                {
+                    nameOK = true;
+                }
+                count++;
+            }
+
+            StreamWriter sw = new StreamWriter(String.Format(fileURL, namecomplement), true);
+            sw.WriteLine(time + "; Status= " + status.ToString("###") + "; UserIP= " + Request.UserHostAddress + "; Message= " + msg + "; URL= " + Request.Url.AbsoluteUri);
+            sw.Flush();
+            sw.Close();
+        }
+
+        private void recordDetail()
+        {
+            //StreamWriter swDetail = new StreamWriter(Configuration.LOGPath + time.Replace(":", "_") + ".config", true);
+            //swDetail.WriteLine("Message:       " + exception.Message);
+            //swDetail.WriteLine("HashCode:      " + exception.GetHashCode());
+            //swDetail.WriteLine("TargetSite:    " + exception.TargetSite);
+            //swDetail.WriteLine("Source:        " + exception.Source);
+            //swDetail.WriteLine("Type:          " + exception.GetType());
+            //swDetail.WriteLine("Data:          ");
+            //swDetail.WriteLine("               " + String.Join(" - ", exception.Data));  // .Aggregate(new StringBuilder(), (sb, x) => sb.Append(x.Key + keyValueSeparator + x.Value + sequenceSeparator), sb => sb.ToString(0, sb.Length - 1)); 
+
+            ////swDetail.WriteLine("Data:          ");
+            ////for (int i = 0; i < exception.Data.Count; i++)
+            ////{
+            ////    swDetail.WriteLine("               " + exception.Data);
+            ////}
+
+            //swDetail.WriteLine("BaseException: " + exception.GetBaseException());
+            //swDetail.WriteLine("StackTrace:");
+            //swDetail.Write(exception.StackTrace);
+            //swDetail.Flush();
+            //swDetail.Close();
+        }
+
+        public string GetErrorMessage()
+        {
+            switch(status)
+            {
+                case 404:
+                    return "We're very sorry we couldn't find the page you are looking for.";
+                default:
+                    return "We're sorry but this is an error page!";
+            }
+        }
+    }
+}
